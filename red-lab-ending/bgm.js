95799e67df0b28ef3f73bc76ff58d82dc6422a23
Kanghoon1204 <21900063@handong.ac.kr>
2026-08-26 15:13:06 +0900

    Polish Red Lab ending narratives

--- red-lab-ending/bgm.js
+++ red-lab-ending/bgm.js
@@ -64,7 +64,8 @@ function renderFlowProgress() {
     'future-d-stop.html': 3,
     'future-e-doctor-hide.html': 3,
     'future-e-outside-hide.html': 3,
-    'future-f-hide.html': 3
+    'future-f-hide.html': 3,
+    'story.html': 4
   };
   const currentStage = stageByPage[filename];
   const header = document.querySelector('.header');
